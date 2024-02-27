cfe0c14fdd9909cf541a0d1b1788423b68dd3ded
Noah Yao <hi@noahyao.me>
2024-02-27 20:17:44 +0800

    Add collate. (#3)
    * gogogo.
    
    * gogogo.
    
    * gogogo.
    
    * gogogo.
    
    * gogogo.
    
    * gogogo.
    
    * gogogo.
    
    * GOGOGO.
    
    * add.
    
    * add.
    
    * add.
    
    * sort and and/or.
    
    * sort and and/or.
    
    * sort and and/or.
    
    * add collate.

--- search/v1_test.go
+++ search/v1_test.go
@@ -1,10 +1,12 @@
 package search
 
 import (
-	"regexp"
+	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/collate"
 )
 
 func TestV1(t *testing.T) {
@@ -29,11 +31,81 @@ func TestV1(t *testing.T) {
 		},
 	})
 
-	response := V1(nil, &V1Request{Index: index, Query: &V1RequestQuery{Raw: "orl", Regs: map[string]*regexp.Regexp{
-		"hello": regexp.MustCompile(`.+rl`),
-	}}})
+	response := V1(nil, &V1Request{Index: index, Query: &V1RequestQuery{}})
 
 	if assert.Equal(t, true, response.Hits.Total > 0) {
 		assert.Equal(t, "123", response.Hits.Hits[0])
 	}
+}
+
+func TestV2(t *testing.T) {
+	request := &V1Request{
+		Query: &V1RequestQuery{
+			SortBys:  "name,description",
+			SortMode: "desc",
+		},
+	}
+
+	var recalls []*V1Doc
+
+	recalls = []*V1Doc{
+		{
+			SortableID: 1,
+			Keywords: map[string]string{
+				"name":        "a",
+				"description": "2",
+			},
+		},
+		{
+			SortableID: 2,
+
+			Keywords: map[string]string{
+				"name":        "a",
+				"description": "1",
+			},
+		},
+	}
+
+	sort.SliceStable(recalls, func(i, j int) bool {
+		for _, sortBy := range strings.Split(request.Query.SortBys, ",") {
+			vi := recalls[i].Keywords[sortBy]
+			vj := recalls[j].Keywords[sortBy]
+
+			if vi == vj {
+				continue
+			}
+
+			if request.Query.SortMode == "asc" {
+				return vi < vj
+			}
+
+			return vi > vj
+		}
+
+		if request.Query.SortMode == "asc" {
+			return recalls[i].SortableID < recalls[j].SortableID
+		}
+
+		return recalls[i].SortableID > recalls[j].SortableID
+	})
+
+	assert.Equal(t, int64(1), recalls[0].SortableID)
+}
+
+func TestCollate(t *testing.T) {
+	collateLess := collate.IndexString("ZH-HANS_CI")
+
+	candidates := []string{"姚", "明", "啊"}
+
+	// Ascending
+	sort.SliceStable(candidates, func(i, j int) bool {
+		return collateLess(candidates[i], candidates[j])
+	})
+	assert.Equal(t, []string{"啊", "明", "姚"}, candidates)
+
+	// Descending
+	sort.SliceStable(candidates, func(i, j int) bool {
+		return !collateLess(candidates[i], candidates[j])
+	})
+	assert.Equal(t, []string{"姚", "明", "啊"}, candidates)
 }
